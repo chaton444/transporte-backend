e2a5510e1231b78c40b2af0976c43ee126ff44bb
chaton444 <chaton444@outlook.com>
2025-02-16 13:58:58 -0600

    habilitar cors para ip publicas

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    // Habilitar CORS y no tener problemas con las peticiones 
    app.enableCors({
-    origin: 'http://localhost:4200', 
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // metodos que se van a usar en un futuro o actualmente para lo clasico de modificarciones de bd
     credentials: true, // para permitir las credenciales para la seguridad
   });
